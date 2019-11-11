5024d10bd87d98dbb2567440660cc0c91433ff1b
Diogo Miguel Melo Magalhães <iminyourpie@gmail.com>
2019-11-11 20:32:03 +0000

    Nav css and toggle open, animations, lading header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 require("./index.css");
+
+window.onload = function() {
+  let menuToggler = document.getElementsByClassName("menu-toggler")[0];
+  let topNav = document.getElementsByClassName("top-nav")[0];
+
+  console.log(menuToggler);
+  menuToggler.addEventListener("click", event => {
+    menuToggler.classList.toggle("open");
+    topNav.classList.toggle("open");
+  });
+};
