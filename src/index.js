c3a2bf49c1652aaa0fe7bc245e091d6fd7ade3b7
Diogo Miguel Melo Magalhães <iminyourpie@gmail.com>
2019-11-13 19:15:54 +0000

    mobile layout, menu interactive

--- src/index.js
+++ src/index.js
@@ -4,9 +4,17 @@ window.onload = function() {
   let menuToggler = document.getElementsByClassName("menu-toggler")[0];
   let topNav = document.getElementsByClassName("top-nav")[0];
 
-  console.log(menuToggler);
   menuToggler.addEventListener("click", event => {
     menuToggler.classList.toggle("open");
     topNav.classList.toggle("open");
+  });
+
+  let navLinks = document.querySelectorAll(".nav-link");
+
+  navLinks.forEach(el => {
+    el.addEventListener("click", event => {
+      menuToggler.classList.toggle("open");
+      topNav.classList.toggle("open");
+    });
   });
 };
