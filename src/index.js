a5f2213dc88661bc991d43da5282cc2b49651033
Diogo Miguel Melo Magalhães <iminyourpie@gmail.com>
2019-11-14 12:40:19 +0000

    Animations, smooth scroll

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import "../dist/style/index.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 window.onload = function() {
+  AOS.init({
+    easing: "ease",
+    duration: 1000
+  });
+
   let menuToggler = document.getElementsByClassName("menu-toggler")[0];
   let topNav = document.getElementsByClassName("top-nav")[0];
+  let up = document.getElementsByClassName("up")[0];
 
-  menuToggler.addEventListener("click", event => {
+  menuToggler.addEventListener("click", () => {
     menuToggler.classList.toggle("open");
     topNav.classList.toggle("open");
   });
@@ -12,9 +20,15 @@ window.onload = function() {
   let navLinks = document.querySelectorAll(".nav-link");
 
   navLinks.forEach(el => {
-    el.addEventListener("click", event => {
+    el.addEventListener("click", () => {
       menuToggler.classList.toggle("open");
       topNav.classList.toggle("open");
+
+      document.getElementById("el.href").scrollIntoView({ behavior: "smooth" });
     });
+  });
+
+  up.addEventListener("click", () => {
+    document.getElementById("home").scrollIntoView({ behavior: "smooth" });
   });
 };
